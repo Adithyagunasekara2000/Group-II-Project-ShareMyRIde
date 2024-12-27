24266b2301d084d73f8e0901f7fda098fe3f63b1
Hiruni Ranasinghe <ranasinghehiruni14@gmail.com>
2024-12-27 13:31:50 +0530

    created the models of ChatbotData.js

---
+++ backend/models/ChatbotData.js
@@ -0,0 +1,21 @@
+const mongoose = requiree("mongoose");
+
+const customMessageSchema = new mongoose.Schema({
+
+    message: {
+        type: String,
+        required: true
+    },
+
+    category: {
+        type: String,
+        required: false
+    },
+
+    createdAt: {
+        type: Date,
+        default: Date.now
+    }
+});
+
+const CustomMessage = mongoose.model("CustomMessage",customMessageSchema);
